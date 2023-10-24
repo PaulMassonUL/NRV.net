b88e70f8f8dc24eed925a81dc87926ba72d86c30
PaulMassonUL <paul.masson1@etu.univ-lorraine.fr>
2023-10-24 20:37:12 +0200

    final sql test data

---
+++ auth.festival/sql/auth.data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `User` (`email`, `password`, `first_name`, `last_name`, `active`)
+VALUES ('user1@example.com', 'password1', 'John', 'Doe', 1),
+       ('user2@example.com', 'password2', 'Alice', 'Smith', 1),
+       ('user3@example.com', 'password3', 'Bob', 'Johnson', 1);
