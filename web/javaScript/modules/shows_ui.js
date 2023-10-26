58d7cc6bccb2856104265b0ad9f68df32c508b5c
mathis54300 <mathis54300fox@gmail.com>
2023-10-26 15:30:09 +0200

    Front ent - Filtre shows

--- web/javaScript/modules/shows_ui.js
+++ web/javaScript/modules/shows_ui.js
@@ -10,14 +10,12 @@ class ShowsUI {
             console.error("Le conteneur des spectacles est introuvable.");
             return;
         }
-
         if (!Array.isArray(shows.shows)) {
             console.error("Les spectacles ne sont pas au format d'un tableau.");
             return;
         }
-
+        showsContainer.innerHTML = '';
         shows.shows.forEach(show => {
-            console.log(show);
             const article = document.createElement('article');
             const liste_artists_name = [];
             show.artists.forEach(artist => {
@@ -41,6 +39,112 @@ class ShowsUI {
             `;
 
             showsContainer.appendChild(article);
+        });
+    }
+
+    render_spots(spots) {
+        const spotsContainer = document.getElementsByClassName('filtre')[0];
+
+        // Vérifiez si l'élément container existe
+        if (!spotsContainer) {
+            console.error("Le conteneur des lieux est introuvable.");
+            return;
+        }
+        //si spotsContainer existe, on vide le contenu
+        spotsContainer.innerHTML = '';
+
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a href="#">Tous les lieux</a>
+        `;
+        li.addEventListener('click', () => {
+            Shows.load_shows();
+        });
+        spotsContainer.appendChild(li);
+
+        spots.spots.forEach(spot => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <a href="#">${spot.name}</a>
+            `;
+            li.addEventListener('click', () => {
+                Shows.load_shows_by_spot(spot.name);
+            });
+            spotsContainer.appendChild(li);
+        });
+    }
+
+    render_dates(dates) {
+        const datesContainer = document.getElementsByClassName('filtre')[0];
+
+        // Vérifiez si l'élément container existe
+        if (!datesContainer) {
+            console.error("Le conteneur des dates est introuvable.");
+            return;
+        }
+        //si datesContainer existe, on vide le contenu
+        datesContainer.innerHTML = '';
+
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a href="#">Tous les jours</a>
+        `;
+        li.addEventListener('click', () => {
+            Shows.load_shows();
+        });
+        datesContainer.appendChild(li);
+
+        dates.dates.forEach(date => {
+            let dateOrigine = date;
+            //transformer date en français
+            const date_li = new Date(date);
+            const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+            date = date_li.toLocaleDateString('fr-FR', options);
+            date = date.charAt(0).toUpperCase() + date.slice(1);
+
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <a href="#">${date}</a>
+            `;
+            //récupérer la date au format yyyy-mm-dd
+            dateOrigine = dateOrigine.slice(0, 10);
+            li.addEventListener('click', () => {
+                Shows.load_shows_by_date(dateOrigine);
+            });
+            datesContainer.appendChild(li);
+        });
+    }
+
+    render_thematics(thematics) {
+        const thematicsContainer = document.getElementsByClassName('filtre')[0];
+
+        // Vérifiez si l'élément container existe
+        if (!thematicsContainer) {
+            console.error("Le conteneur des thématiques est introuvable.");
+            return;
+        }
+
+        //si thematicsContainer existe, on vide le contenu
+        thematicsContainer.innerHTML = '';
+
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <a href="#">Tous les styles</a>
+        `;
+        li.addEventListener('click', () => {
+            Shows.load_shows();
+        });
+        thematicsContainer.appendChild(li);
+
+        thematics.thematics.forEach(thematic => {
+            const li = document.createElement('li');
+            li.innerHTML = `
+            <a href="#">${thematic}</a>
+            `;
+            li.addEventListener('click', () => {
+                Shows.load_shows_by_thematic(thematic);
+            });
+            thematicsContainer.appendChild(li);
         });
     }
 
