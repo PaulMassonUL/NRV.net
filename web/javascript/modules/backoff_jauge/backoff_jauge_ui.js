ebcc73c5b0eaf5202a65bb3c6719b195e1f679fb
Yvan <yvanquilliard@hotmail.fr>
2023-10-27 17:22:44 +0200

    create ajax for jauge backoffice

---
+++ web/javascript/modules/backoff_jauge/backoff_jauge_ui.js
@@ -0,0 +1,23 @@
+import BackoffJauge from './backoff_jauge.js';
+
+class Backoff_jauge_ui {
+    render_places(places) {
+        // const placesContainer = document.getElementsByClassName('backoff_jauge')[0];
+
+        console.log(places);
+        // Vérifiez si l'élément container existe
+        // if (!placesContainer) {
+        //     console.error("Le conteneur des places est introuvable.");
+        //     return;
+        // }
+        // //si placesContainer existe, on vide le contenu
+        // placesContainer.innerHTML = '';
+        //
+        // const article = document.createElement('article');
+        // article.innerHTML = `
+        //     <p>${places.places}</p>
+        // `;
+        //
+        // placesContainer.appendChild(article);
+    }
+}
