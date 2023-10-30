0d329cdabc200b0ea14070eac3cc1e3547463f34
vavar <vavasseu2u@etu.univ-lorraine.fr>
2023-10-30 01:39:36 +0100

    changement de la page de connexion/inscription

--- web/javascript/connexion.js
+++ web/javascript/connexion.js
@@ -5,3 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     connexionForm.initializeEventListeners();
 
 });
+
+const container = document.getElementById('container');
+
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    console.log("login");
+    container.classList.remove("active");
+});
